93176cb4cd881a756ca6805d30c6156fae6274ba
JoelBal <gmail>
2021-04-21 11:18:55 -0400

    characterarrays

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
+var LowerCaseChar = ["a", "b", "c", "d", "e", "g", "h", "i", "j", "k", "l", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var UpperCaseChar = ["A", "B", "C", "D", "E", "G", "H", "I", "J", "K", "L", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var SymbolsChar = ["!", "@", "#", "$", "%", "^", "&", "*", "~", "+", "=", "?"];
+var NumbersChar = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
